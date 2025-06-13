bf65eb892a1ae2e36f83792264bb17bf9aca29d1
keiseiisobe <keisei.isobe@gmail.com>
2025-06-13 19:46:24 +0900

    added weather_app

--- mobileModule01/weather_app/lib/main.dart
+++ mobileModule01/weather_app/lib/main.dart
@@ -1,9 +1,12 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:window_size/window_size.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();  
-  setWindowMinSize(Size(250, 400));
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    setWindowMinSize(Size(300, 400));
+  }
   runApp(const MyApp());
 }
 
@@ -32,148 +35,194 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  PageController pageController = PageController(
-    initialPage: 0,
-  );  
-  var pageSelected = 0;
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+  late final TabController tabController;
+  final TextEditingController searchController = TextEditingController();
+  var isGeoLocationEnabled = false;
+  var isSearchLocationEnabled = false;
 
   List<Widget> getBottomNavigationBarItems() {
     return [
-      Expanded(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-              flex: 2,  
-              child: IconButton(
-                isSelected: pageSelected == 0,
-                icon: const Icon(Icons.wb_sunny),
-                onPressed: () {
-                  setState(() {
-                    pageSelected = 0;  
-                  });  
-                  pageController.jumpToPage(0);  
-                },
-              )
-            ),
-            Expanded(child: Text("Currently"))
-          ],
+      Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+            Expanded(child: const Icon(Icons.wb_sunny)),
+            SizedBox(height: 4),
+            Expanded(child: Text("Currently")),
+        ],
+      ),
+      Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+            Expanded(child: const Icon(Icons.calendar_today)),
+            SizedBox(height: 4),
+            Expanded(child: Text("Today")),
+        ],
+      ),
+      Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+            Expanded(child: const Icon(Icons.calendar_view_week)),
+            SizedBox(height: 4),
+            Expanded(child: Text("Weekly")),
+        ],
+      ),
+    ];
+  }
+
+  List<Widget> getTopBarItems() {
+    return [
+      Container(
+        width: MediaQuery.of(context).size.width * 0.8,
+        alignment: Alignment.bottomLeft,
+        child: TextField(
+          controller: searchController, 
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.onPrimary,
+          ),
+          cursorColor: Theme.of(context).colorScheme.onPrimary,
+          decoration: InputDecoration(
+            hintText: "Search location...",
+            hintStyle: TextStyle(color: Theme.of(context).colorScheme.primaryContainer),
+            border: InputBorder.none,
+            prefixIcon: Icon(Icons.search),
+            prefixIconColor: Theme.of(context).colorScheme.primaryContainer,
+            counterText: "",
+          ),
+          maxLength: 100,
+          onSubmitted: (value) {
+            setState(() {
+              isSearchLocationEnabled = true;
+              isGeoLocationEnabled = false;
+            });  
+          },
         ),
       ),
-      Expanded(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-              flex: 2,  
-              child: IconButton(
-                isSelected: pageSelected == 1,  
-                icon: const Icon(Icons.calendar_today),
-                onPressed: () {
-                  setState(() {
-                    pageSelected = 1;
-                  });  
-                  pageController.jumpToPage(1);
-                },
-              )
-            ),
-            Expanded(child: Text("Today"))
-          ],
-        ),
+      VerticalDivider(
+        color: Theme.of(context).colorScheme.onPrimary,
+        indent: 10,
+        endIndent: 10,
+        width: 0,
       ),
-      Expanded(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-              flex: 2,  
-              child: IconButton(
-                isSelected: pageSelected == 2,  
-                icon: const Icon(Icons.calendar_view_week),
-                onPressed: () {
-                  setState(() {
-                    pageSelected = 2;
-                  });  
-                  pageController.jumpToPage(2);  
-                },
-              )
-            ),  
-            Expanded(child: Text("Weekly"))
-          ],
+      Container(
+        width: MediaQuery.of(context).size.width * 0.2,
+        alignment: Alignment.center,
+        child: IconButton(
+          onPressed: () {
+            setState(() {
+              if (isGeoLocationEnabled) {
+                isGeoLocationEnabled = false;
+              } else {
+                isGeoLocationEnabled = true;
+                isSearchLocationEnabled = false;
+              }  
+            });  
+          },
+          icon: Icon(
+            Icons.near_me,
+            color: Theme.of(context).colorScheme.onPrimary,
+          ),
+          constraints: BoxConstraints.expand(),
         ),
       ),
     ];
   }
 
   @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    tabController.dispose();  
+    searchController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.secondary,  
-          flexibleSpace: TabBar(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.secondary,  
+        flexibleSpace: SafeArea(
+          child: TabBar(
+            controller: tabController,  
             tabAlignment: TabAlignment.start,  
+            labelPadding: EdgeInsets.only(top: 8),
             isScrollable: true,
-            labelPadding: EdgeInsets.zero,
-            indicatorPadding: EdgeInsets.symmetric(vertical: 10),
-            indicator: ShapeDecoration(shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(100.0)),
-            )),
-
-
-            tabs: [
-              Container(
-                width: MediaQuery.of(context).size.width * 0.8,
-                alignment: Alignment.bottomCenter,
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Search location...",
-                    hintStyle: TextStyle(color: Theme.of(context).colorScheme.primaryContainer),
-                    border: InputBorder.none,
-                    prefixIcon: Icon(Icons.search),
-                    prefixIconColor: Theme.of(context).colorScheme.primaryContainer,
-                  ),
-                ),
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width * 0.2,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  border: Border(
-                    left: BorderSide(
-                      color: Theme.of(context).colorScheme.onPrimary,
-                      width: 2.0,  
-                      style: BorderStyle.solid
-                    ),
-                  ),
-                ),
-                child: IconButton(
-                  icon: Icon(
-                    Icons.near_me,
-                    color: Theme.of(context).colorScheme.onPrimary,
-                  ),
-                  onPressed: () {
-                    // Action for the near me button
-                  },
-                ),
-              ),
-            ],  
+            dividerColor: Colors.transparent,
+            indicatorColor: Colors.transparent,
+            labelColor: Colors.transparent,
+            unselectedLabelColor: Colors.transparent,
+            overlayColor: WidgetStateProperty.all(Colors.transparent),
+            mouseCursor: SystemMouseCursors.basic,
+            tabs: getTopBarItems(),
+            onTap: (index) {
+              setState(() {
+                tabController.index = tabController.previousIndex;
+              });
+            },
           ),
-        ),  
-        bottomNavigationBar: BottomAppBar(
-          child: Row(children: getBottomNavigationBarItems()),
         ),
-        body: PageView(
-          controller: pageController,
-          children: [
-            Center(child: Text("Currently")),
-            Center(child: Text("Today")),
-            Center(child: Text("Weekly")),
-          ],
+      ),  
+      bottomNavigationBar: BottomAppBar(
+        child: TabBar(
+          controller: tabController,  
+          tabs: getBottomNavigationBarItems()
         ),
-      )
+      ),
+      body: SafeArea(
+        child: DefaultTextStyle(
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+          ),  
+          child: TabBarView(
+            controller: tabController,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,  
+                children: [
+                  Text("Currently"),
+                  isGeoLocationEnabled 
+                    ? Text("Geolocation") 
+                    : isSearchLocationEnabled 
+                      ? Text(searchController.text) 
+                      : Text(""),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,  
+                children: [
+                  Text("Today"),  
+                  isGeoLocationEnabled 
+                    ? Text("Geolocation") 
+                    : isSearchLocationEnabled 
+                      ? Text(searchController.text) 
+                      : Text(""),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,  
+                children: [
+                  Text("Weekly"),  
+                  isGeoLocationEnabled 
+                    ? Text("Geolocation") 
+                    : isSearchLocationEnabled 
+                      ? Text(searchController.text) 
+                      : Text(""),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
